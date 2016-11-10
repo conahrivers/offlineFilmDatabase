d41ae4c8d4c61d1bfef86c607d67d6e85f61e669
conahrivers <conahcharlierivers@gmail.com>
2016-11-10 13:44:16 +0000

    Update main.c

--- main.c
+++ main.c
@@ -20,17 +20,19 @@
 int main(int argc, char** argv) {
     
     FILE *filmFile;
-    char buf[8000];
-
     filmFile = fopen("films.txt", "r");
-    if (!filmFile)
-        return 1;
-
-    while (fgets(buf, 8000, filmFile) != NULL)
-        printf("%s", buf);
-
+    char c;
+    
+    c = fget(filmFile);
+    
+    while(c=fgetc(filmFile) ) != EOF) {
+        printf("%c", c);
+    }
+    
+    printf("\n End of list\n");
+    
     fclose(filmFile);
-    return 0;
+    
 
     return (EXIT_SUCCESS);
 }
