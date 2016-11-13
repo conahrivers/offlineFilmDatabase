a37d0dadb743e6a1e419247393a336327d89c1ba
conahrivers <conahcharlierivers@gmail.com>
2016-11-13 22:30:42 +0000

    Update main.c

--- main.c
+++ main.c
@@ -14,26 +14,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+#include "main.h"
+
+/***************************************************************/
+typedef struct filmList {
+  
+  char title [50];
+  int releaseYear[4];                                                            //creating struct with fields
+  char rating[4];
+  char genre[20];
+  int runTime[3];
+  double rating[3];
+  struct filmList *next; //self referential structure
+  
+} Node;
+/***************************************************************/
+
 int main(int argc, char** argv) {
     
-    FILE *filmFile;
-    filmFile = fopen("films.txt", "r");
-    char c;
     
-    c = fget(filmFile);
     
-    while(c=fgetc(filmFile) ) != EOF) {
-        printf("%c", c);
-    }
-    
-    printf("\n End of list\n");
-    
-    fclose(filmFile);
-    
+/***************************************************************/
+    FILE *pToFile = fopen("films.txt", "r");
 
+    int line = 0;
+
+    char input[800];
+
+    while (fgets(input, 800, pToFile)) {
+        line++;
+        printf("Line: %d -> %s", line, input);                                   //opens the file
+
+        if (pToFile == NULL) {
+            printf("Error: unable to open 'films.txt' in mode 'r' \n");
+
+            exit(EXIT_FAILURE);
+        }
+    }
+    printf("\n\n End program\n");
+
+    fclose(pToFile);
+/***************************************************************/
     return (EXIT_SUCCESS);
 }
 
+/** 
+ * FILE *pToFile = fopen("films.txt", "r");
+
+    int line = 0;
+
+    char input[800];
+
+    while (fgets(input, 800, pToFile)) {
+        line++;
+        printf("Line: %d -> %s", line, input);
+
+        if (pToFile == NULL) {
+            printf("Error: unable to open 'films.txt' in mode 'r' \n");
+
+            exit(EXIT_FAILURE);
+        }
+    }
+    printf("\n\n End program\n");
+
+    fclose(pToFile);
+ **/
