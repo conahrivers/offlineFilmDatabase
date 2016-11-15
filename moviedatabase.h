b31bfbf06414154b00eec48dc85af38638c00415
conahrivers <conahcharlierivers@gmail.com>
2016-11-15 18:26:20 +0000

    Update moviedatabase.h

--- moviedatabase.h
+++ moviedatabase.h
@@ -6,9 +6,9 @@
 
 /* 
  * File:   moviedatabase.h
- * Author: ebx15jtu
+ * Author: conah
  *
- * Created on 15 November 2016, 11:56
+ * Created on 10 November 2016, 12:06
  */
 
 #ifndef MOVIEDATABASE_H
@@ -18,16 +18,18 @@
 extern "C" {
 #endif
 
-typedef struct filmList {
-    char title [50];
-    int releaseYear[4]; //creating struct with fields
-    char agerating[4];
-    char genre[20];
-    int runTime[3];
-    double rating [3];
-    struct filmList *next; //points to dummy node
-
+typedef struct filmlist {
+  
+  char title [50];
+  int releaseYear[4];
+  char rating[4];
+  char genre[20];
+  int runTime[3];
+  double rating[3];
+  
 } films;
+
+// linked list goes here
 
 
 #ifdef __cplusplus
