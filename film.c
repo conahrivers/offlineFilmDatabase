ba1b7508fb4c6d44b34680f5cdde13afec86165e
conahrivers <conahcharlierivers@gmail.com>
2016-11-15 18:24:09 +0000

    Update film.c

--- film.c
+++ film.c
@@ -1,34 +1,56 @@
-/*
- *start of a linked list for film database
- */
-
+#include <stdio.h>
 #include <stdlib.h>
 
+void printfuction() {
 
-struct filmList {
-    
+    /***************************************************************/
+
     char title [50];
-    int releaseYear[4]; //creating struct with fields
-    char agerating[4];
-    char genre[20];
-    int runTime[3];
-    double rating [3];
-    struct filmList *next; //points to dummy node
+    int releaseYear;
+    char ageRating [50];
+    char genre [50];
+    int runTime;
+    float rating ;
 
-};
+    /***************************************************************/
+    FILE *pToFile = fopen("films.txt", "r");
 
-int main() {
-    
-struct filmList *root; //first node
+    int count = 0;
 
-root = (struct filmList *) malloc(sizeof(struct filmList)); //root pointer to a node in struct
+    char line [800];
 
-root ->next; //root points to the next node which is null
-root ->title;
-root ->releaseYear;
-root ->agerating;
-root ->genre;
-root ->runTime;
-root ->rating;
+    /***************************************************************/
 
-}
+    while (fgets(line, sizeof (line), pToFile) != NULL) {
+        count++;
+       // printf("%s", line); //opens the file
+
+         sscanf(line, "\"%[^\"]\", %d, \"%[^\"]\", \"%[^\"]\", %d, %f", title, &releaseYear, ageRating, genre, &runTime, &rating);
+         
+         printf("Title: %s \n" 
+                "Release Year: %d \n"
+                "Age Rating: %s \n"
+                "Genre: %s \n"
+                "Run Time: %d \n"
+                "Rating: %.1f \n "
+                 "\n -----------------------\n", title, releaseYear, ageRating, genre, 
+                runTime, rating
+                 );
+
+        if (pToFile == NULL) {
+            printf("Error: unable to open 'films.txt' in mode 'r' \n");
+
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /***************************************************************/
+
+    printf("\n Total fils: %d", count);
+    printf("\n\n End program\n");
+
+    fclose(pToFile);
+
+    /***************************************************************/
+    }
+
