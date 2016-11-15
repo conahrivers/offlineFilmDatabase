de17278d1c403706f45d6abb0d2895a15758b33f
conahrivers <conahcharlierivers@gmail.com>
2016-11-15 11:47:16 +0000

    Update film.c

--- film.c
+++ film.c
@@ -1,6 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *start of a linked list for film database
  */
 
+#include <stdlib.h>
+
+
+struct filmList {
+    
+    char title [50];
+    int releaseYear[4]; //creating struct with fields
+    char agerating[4];
+    char genre[20];
+    int runTime[3];
+    double rating [3];
+    struct filmList *next; //points to dummy node
+
+};
+
+int main() {
+    
+struct filmList *root; //first node
+
+root = (struct filmList *) malloc(sizeof(struct filmList)); //root pointer to a node in struct
+
+root ->next; //root points to the next node which is null
+root ->title;
+root ->releaseYear;
+root ->agerating;
+root ->genre;
+root ->runTime;
+root ->rating;
+
+}
