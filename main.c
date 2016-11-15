3de90bd4c7b5179442e54b6c1c9e5543831451a1
conahrivers <conahcharlierivers@gmail.com>
2016-11-15 18:32:37 +0000

    Update main.c

--- main.c
+++ main.c
@@ -15,18 +15,6 @@
 #include <stdlib.h>
 
 /***************************************************************/
-/** typedef struct filmList {
-    char title [50];
-    int releaseYear[4]; //creating struct with fields
-    char agerating[4];
-    char genre[20];
-    int runTime[3];
-    double rating [3];
-    struct filmList *next; //points to dummy node
-
-} films; **/
-
-/***************************************************************/
 
 int main(int argc, char** argv) {
     
